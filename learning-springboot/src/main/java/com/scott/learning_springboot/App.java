5a8144bc6fad955c91064f0bbd9bd0983fdfbeec
Scott <sco.suen.lab@gmail.com>
2016-03-29 14:38:07 -0400

    feat:initialize project

---
+++ learning-springboot/src/main/java/com/scott/learning_springboot/App.java
@@ -0,0 +1,20 @@
+package com.scott.learning_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Hello world!
+ *
+ */
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		SpringApplication.run(App.class);
+	}
+}
---
+++ learning-springboot/src/main/java/com/scott/learning_springboot/UserController.java
@@ -0,0 +1,18 @@
+package com.scott.learning_springboot;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+	@RequestMapping("/{id}")
+	public User view(@PathVariable("id") Long id) {
+		User user = new User();
+		user.setId(id);
+		user.setName("zhang");
+		return user;
+	}
+}
